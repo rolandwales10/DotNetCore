c2744e135867f6c62152917e780b2ab1c650f5a4
Roland Wales <roland.wales@maine.gov>
2022-07-14 08:37:35 -0400

    calling Javascript from farm allocation.manage

--- FarmshareAdmin/wwwroot/js/site2.js
+++ FarmshareAdmin/wwwroot/js/site2.js
@@ -2,6 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-function site() {
-    alert('site');
+function site2() {
+    alert('site2');
 }
